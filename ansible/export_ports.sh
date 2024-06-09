42be002ead86bef19864bf969685859d95e4b7bb
BachErik <75324423+BachErik@users.noreply.github.com>
2024-06-09 10:10:26 +0200

    fix: exposed ports

---
+++ ansible/export_ports.sh
@@ -0,0 +1,13 @@
+apt update
+apt upgrade
+apt install ufw -y
+ufw allow 22/tcp
+ufw allow 6443/tcp
+ufw allow 80/tcp
+ufw allow 443/tcp
+ufw allow 10250/tcp
+ufw deny 10251/tcp
+ufw deny 10252/tcp
+ufw allow 8285/udp
+ufw allow 8472/udp
+ufw enable
